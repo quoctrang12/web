6f91e1fc9e8c1d46d283a33d08355468a5eb3140
quoctrang12 <92104941+quoctrang12@users.noreply.github.com>
2021-10-09 22:15:37 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,7 +10,30 @@ $(document).ready(
                 $('.icon-sub-menu').addClass('fa-chevron-down');
                 $('.icon-sub-menu').removeClass('fa-chevron-up');
             }
-        }
-        
+        }   
     )
+    
 )
+
+tabs = ()=>{
+    var items = document.querySelectorAll(".item");
+    var contents = document.querySelectorAll(".img-main");
+
+    items.forEach((item,index)=>{
+        item.addEventListener("click",()=>{
+            removeClass();
+            items[index].classList.add("active");
+            contents[index].classList.add("active");
+        })
+    })
+    
+    removeClass = () => {
+        items.forEach((item,index)=>{
+                items[index].classList.remove("active");
+                contents[index].classList.remove("active");
+            })
+    }
+}
+tabs();
+
+
